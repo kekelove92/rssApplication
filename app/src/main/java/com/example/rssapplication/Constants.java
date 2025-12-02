867f13e5a042566a03eaebe4df441c31309cdc59
Tung Anh <zatbum@gmail.com>
2025-12-02 22:05:24 +0700

    update code

---
+++ app/src/main/java/com/example/rssapplication/Constants.java
@@ -0,0 +1,11 @@
+package com.example.rssapplication;
+
+public class Constants {
+    public static final String[] CATEGORIES = {
+            "Tất cả",
+            "Tin nổi bật",
+            "Thế giới",
+            "Thể thao"
+    };
+}
+
